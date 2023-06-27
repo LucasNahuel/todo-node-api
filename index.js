242408725415b939fee4ec191586b8a837e4f191
LucasNahuel <munozlucas96@gmail.com>
2023-06-26 21:36:00 -0300

    included mongo driver and dotenv, configured environment variables and mongo connection

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
-const port = 4300;
+
+//config dotenv to read environment variables from .env file
+dotenv.config();
+
+
+const port = process.env.PORT;
+
+
+//initialize mongo driver, define database
+const { MongoClient } = require('mongodb');
+
+//mongo connections properties
+const username = process.env.ATLAS_USER;
+const password = process.env.ATLAS_PASSWORD;
+const clusterUrl = "cluster0.ypdud.mongodb.net";
+const authMechanism = "DEFAULT";
+const connectionString = `mongodb+srv://${username}:${password}@${clusterUrl}/?authMechanism=${authMechanism}`;
+
+const client = new MongoClient(connectionString);
+
+let conn;
+
+let db;
+
+async function runMongo(){
+    conn = await client.connect();
+}
+
+runMongo().then(()=>{
+        db = conn.db("todo-db");
+    }).catch(
+    error => {
+        console.log(error);
+    }
+);
+
+
+
+
+
+
+
+
+//define endpoints for express
 
 app.get('/', (req, res) => {
     res.send('todo api works!');
